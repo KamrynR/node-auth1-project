eed01bb5055790834798facdfa2ae0ba1b6ea3be
KamrynR <62856359+KamrynR@users.noreply.github.com>
2021-01-12 23:32:10 -0600

    mvp

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const helmet = require("helmet")
+const cors = require("cors")
+const session = require("express-session")
+const ConnectSessionKnex = require('connect-session-knex')(session)
+const usersRouter = require("./users/users-router")
+const db = require('./database/config')
+
+const server = express()
+const port = process.env.PORT || 8080
+
+server.use(helmet())
+server.use(cors())
+server.use(express.json())
+server.use(session({
+	resave: false,
+	saveUninitialized: false,
+	secret: 'secret',
+	store: new ConnectSessionKnex({
+		knex: db,
+		createtable: true,
+	}),
+}))
+
+server.use('/api', usersRouter)
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
+
+server.listen(port, () => {
+	console.log(`Running at http://localhost:${port}`)
+})
